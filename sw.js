2649367b67abbc33b3bd94e3a39ce358d6bce9b5
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-09-15 13:43:44 +0200

    fucking cache

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const cacheList = {
     ]
   },
   'audio-player': {
-    version: '1.0.4',
+    version: '1.0.5',
     resources: [
       '../audio-player/public/build/bundle.js',
       '../audio-player/public/build/bundle.js.map',
