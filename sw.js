bff6c2c0727389dfd58efe946950e0261716429d
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-09-15 16:38:08 +0200

    yup.

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const cacheList = {
     ]
   },
   'audio-player': {
-    version: '1.0.8',
+    version: '1.0.9',
     resources: [
       '../audio-player/public/build/bundle.js',
       '../audio-player/public/build/bundle.js.map',
