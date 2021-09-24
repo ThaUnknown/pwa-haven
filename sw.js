86043f582ffe39c80e15d738ceedf3016ed18d50
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-09-24 19:50:42 +0200

    I'd never forget to build

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const cacheList = {
     ]
   },
   'video-player': {
-    version: '1.2.6',
+    version: '1.2.7',
     resources: [
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.bundle.min.js',
       '../video-player/public/lib/subtitles-octopus-worker.js',
