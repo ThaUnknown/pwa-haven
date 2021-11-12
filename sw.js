c7996b0a45fd0ac99b7dc11676804da00496c540
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-11-12 20:10:11 +0100

    haha!

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const cacheList = {
     ]
   },
   'video-player': {
-    version: '1.6.2',
+    version: '1.7.0',
     resources: [
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.bundle.min.js',
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.wasm',
