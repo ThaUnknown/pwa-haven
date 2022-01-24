017da8d3292730eb5f4767c0aecc95ca163e97b3
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-01-24 12:20:07 +0100

    bump ver

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const cacheList = {
     ]
   },
   'video-player': {
-    version: '1.9.10',
+    version: '1.10.0',
     resources: [
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.bundle.min.js',
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.wasm',
