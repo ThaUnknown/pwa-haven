6b85e21f5b54bc4c139e481b4a6f75046786673a
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-02-25 03:25:25 +0100

    forgot build

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const cacheList = {
     ]
   },
   'video-player': {
-    version: '1.11.17',
+    version: '1.11.18',
     resources: [
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.bundle.min.js',
       'https://cdn.jsdelivr.net/npm/anitomyscript@2.0.4/dist/anitomyscript.wasm',
