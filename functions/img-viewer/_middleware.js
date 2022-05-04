e33866eea3c1d1f6e2232ec2becb50ce59cf7a32
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-05-04 15:43:09 +0200

    feat: add slowpics redirect

---
+++ functions/img-viewer/_middleware.js
@@ -0,0 +1,22 @@
+export async function onRequest ({ request, next }) {
+  const url = new URL(request.url)
+  if (url.pathname.startsWith('/img-viewer/https://slow.pics/c/')) {
+    const res = await fetch(url.pathname.split('/img-viewer/')[1])
+    const text = await res.text()
+    const match = text.match(/var collection = (.*);\n/)
+    if (!match[1]) return next()
+    const obj = JSON.parse(match[1])
+
+    let destinationURL = url.origin + '/img-viewer/public/?'
+
+    let i = 0
+    for (const comparison of obj.comparisons) {
+      for (const image of comparison.images) {
+        destinationURL += i++ + '=' + 'https://i.slow.pics/' + image.publicFileName + '&'
+      }
+    }
+    return Response.redirect(destinationURL, 301)
+  } else {
+    return next()
+  }
+}
