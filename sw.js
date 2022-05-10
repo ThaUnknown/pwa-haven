bd1676ebf3b4572576ccd1bf53f4ffddc1c04e7f
Cas <6506529+ThaUnknown@users.noreply.github.com>
2022-05-10 22:11:17 +0200

    chore: bump ver

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const cacheList = {
     ]
   },
   'audio-player': {
-    version: '1.8.1',
+    version: '1.8.2',
     resources: [
       '../audio-player/public/build/bundle.js',
       '../audio-player/public/build/bundle.css',
